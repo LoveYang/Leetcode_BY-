1ab040b484df7e055884230c1470c9607570770e
LoveYang <13817795692@163.com>
2017-06-27 22:55:45 +0800

    merge

---
+++ Merge Two Binary Trees/solution.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    def __repr__(self):
+        return 'TreeNode({})'.format(self.val)
+def deserialize(string):
+    if string == '{}':
+        return None
+    nodes = [None if val == 'null' else TreeNode(int(val))
+             for val in string.strip('[]{}').split(',')]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids: node.left  = kids.pop()
+            if kids: node.right = kids.pop()
+    return root
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        if t1 and t2:
+            root = TreeNode(t1.val + t2.val)
+            root.left = self.mergeTrees(t1.left, t2.left)
+            root.right = self.mergeTrees(t1.right, t2.right)
+            return root
+        else:
+            return t1 or t2
+if __name__ == '__main__':
+    t1=deserialize('[1,3,2,5]')
+    t2=deserialize('[2,1,3,null,4,null,7]')
+    sov=Solution()
+    mer=sov.mergeTrees(t1,t2)
